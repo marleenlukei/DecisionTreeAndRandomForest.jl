13e365c073ae1885e2304d584c9cad532a663e21
enea09 <e.gurra@campus.tu-berlin.de>
2024-06-08 20:02:38 +0200

    add test case

--- test/test_classificationtree.jl
+++ test/test_classificationtree.jl
@@ -1,5 +1,7 @@
 using DecisionTreeAndRandomForest
 using Test
+using MLJ
+using DataFrames
 
 @testset "ClassificationTree" begin
     data = ["dog" 37.0; "dog" 38.4; "dog" 40.2; "dog" 38.9; "human" 36.2; "human" 37.4; "human" 38.8; "human" 36.2]
@@ -13,4 +15,21 @@ using Test
     print_tree(tree)
     @test prediction[1] == "healthy"
     @test prediction[2] == "sick"
+
+    data = load_iris()
+	iris = DataFrame(data)
+	y, X = unpack(iris, ==(:target); rng=123)
+	train, test = partition(eachindex(y), 0.7)
+	train_labels = Vector{String}(y[train])
+	test_labels = Vector{String}(y[test])
+	train_data = Matrix(X[train, :])
+	test_data = Matrix(X[test, :])
+    tree = ClassificationTree(train_data, train_labels)
+    fit(tree)
+    predictions = predict(tree, test_data)
+    print_tree(tree)
+    accuracy = sum(predictions .== test_labels) / length(test_labels)
+    @test Set(predictions) <= Set(test_labels)
+    @test accuracy >= 0.90
+
 end
