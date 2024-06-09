7c9d53063ab00c6de8d837322e2430a61ee56dac
Cedric Lenßen <cedric.lenssen@gmail.com>
2024-06-09 14:21:13 +0200

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,14 @@ using DecisionTreeAndRandomForest
 DocMeta.setdocmeta!(DecisionTreeAndRandomForest, :DocTestSetup, :(using DecisionTreeAndRandomForest); recursive=true)
 
 makedocs(
-    sitename="DecisionTreeAndRandomForest.jl",
-    authors="Marleen Lukei <marleen.lukei@campus.tu-berlin.de>, Cedric Lenßen <c.lenssen@campus.tu-berlin.de>, Arkadeep Dutta, Enea Gurra",
     modules=[DecisionTreeAndRandomForest],
+    authors="Marleen Lukei <marleen.lukei@campus.tu-berlin.de>, Cedric Lenßen <c.lenssen@campus.tu-berlin.de>, Arkadeep Dutta, Enea Gurra",
+    sitename="DecisionTreeAndRandomForest.jl",
+    format=Documenter.HTML(;
+        canonical="https://marleenlukei.github.io/DecisionTreeAndRandomForest.jl",
+        edit_link="main",
+        assets=String[],
+    ),
     pages=[
         "Home" => "index.md",
         "Getting Started" => "getting_started.md",
@@ -15,4 +20,5 @@ makedocs(
 
 deploydocs(;
     repo="github.com/marleenlukei/DecisionTreeAndRandomForest.jl",
+    devbranch="main",
 )
