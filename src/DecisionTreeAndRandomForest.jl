eaa60811f3d86b943a24f0e8e894ed6e13134579
arkadeep16121993 <arkadeeprulez@gmail.com>
2024-06-12 12:41:00 +0200

    Split Crierion Function

--- src/DecisionTreeAndRandomForest.jl
+++ src/DecisionTreeAndRandomForest.jl
@@ -6,6 +6,6 @@ module DecisionTreeAndRandomForest
 include("ClassificationTree.jl")
 include("GiniImpurity.jl")
 include("InformationGain.jl")
-
+include("SplitCriterion.jl")
 export ClassificationTree, fit, predict, print_tree, find_best_split
 end
