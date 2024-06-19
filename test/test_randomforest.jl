ad09cfec6006d2fd91b2be0465474e28678387d0
enea09 <e.gurra@campus.tu-berlin.de>
2024-06-19 11:35:03 +0200

    add iris dataset test

--- test/test_randomforest.jl
+++ test/test_randomforest.jl
@@ -1,5 +1,7 @@
 using DecisionTreeAndRandomForest
 using Test
+using DataFrames
+using MLJ: load_iris, unpack, partition
 
 @testset "RandomForest" begin
     
@@ -13,5 +15,20 @@ using Test
     @test prediction[1] == "healthy"
     @test prediction[2] == "sick"
 
+
+    data = load_iris()
+	iris = DataFrame(data)
+	y, X = unpack(iris, ==(:target); rng=123)
+	train, test = partition(eachindex(y), 0.7)
+	train_labels = Vector{String}(y[train])
+	test_labels = Vector{String}(y[test])
+	train_data = Matrix(X[train, :])
+	test_data = Matrix(X[test, :])
+    rf = RandomForest(10, -1, 2, size(train_data, 2), train_data, train_labels)
+    fit_forest(rf)
+    predictions = predict_forest(rf, test_data)
+    accuracy = sum(predictions .== test_labels) / length(test_labels)
+    @test Set(predictions) <= Set(test_labels)
+    @test accuracy >= 0.90
 
 end
